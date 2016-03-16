2ae8baa558c49fe74c8564d33fba863c6c19f363
Paul Cowie <paul.cowie@ntlworld.com>
2016-03-16 20:24:39 +0000

    Add csv option

--- app/blood_levels.py
+++ app/blood_levels.py
@@ -1,8 +1,15 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
+import argparse
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Produce a command line view of the current Scotblood blood levels')
+    parser.add_argument('--csv', dest='is_csv', action='store_const',
+                        const=True, default=False, help='display results in csv format rather than a bar chart')
+
+    args = parser.parse_args()
+
     url = 'https://www.scotblood.co.uk/learn-about-blood/current-blood-stock-levels.aspx'
     request = urllib2.Request(url, headers={
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'})
@@ -13,13 +20,20 @@ def main():
 
     len_longest_blood_type = 3
 
+    if args.is_csv:
+        print "blood_type,days_left"
+
     for stock_level in stock:
         stock_data = stock_level['title'].split()
         blood_type = stock_data[0]
         blood_stock_level = stock_data[5]
+        
+        if args.is_csv:
+            print blood_type + ',' + blood_stock_level
 
-        print blood_type + ' ' * (len_longest_blood_type - len(blood_type)) + \
-            get_bar(float(blood_stock_level)) + ' | ' + blood_stock_level + ' days left'
+        else:
+            print blood_type + ' ' * (len_longest_blood_type - len(blood_type)) + \
+                get_bar(float(blood_stock_level)) + ' | ' + blood_stock_level + ' days left'
 
 
 def get_bar(blood_stock_level):
