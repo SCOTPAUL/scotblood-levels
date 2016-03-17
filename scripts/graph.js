8422fb5c439fe2c89ef02817f616b22d3ba6576d
Paul Cowie <paul.cowie@ntlworld.com>
2016-03-17 22:59:01 +0000

    Working graph

--- scripts/graph.js
+++ scripts/graph.js
@@ -5,10 +5,9 @@ $(document).ready(function(){
   $.get( "{{ "/_data/blood_stats.csv" | prepend: site.baseurl }}",
     function startgraph(data){
       var res = $.csv.toObjects(data);
-      console.log(res)
       nv.addGraph(function() {
         var chart = nv.models.lineChart()
-                      .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
+                      .margin({left: 100, right:50})  //Adjust chart margins to give the x-axis some breathing room.
                       .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
                       .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
                       .showYAxis(true)        //Show the y-axis
@@ -20,12 +19,12 @@ $(document).ready(function(){
             .tickFormat(function(d) { return d3.time.format('%Y-%m-%d')(new Date(d)); });
     
         chart.yAxis     //Chart y-axis settings
-            .axisLabel('Voltage (v)')
-            .tickFormat(d3.format('.02f'));
+            .axisLabel('Days Remaining')
+            .tickFormat(d3.format('.1f'));
     
         /* Done setting the chart up? Time to render it!*/
-        var myData = sinAndCos();   //You need data...
-    
+        var myData = getData(res);   //You need data...
+
         d3.select('#chart svg')    //Select the &lt;svg&gt; element you want to render the chart in.   
             .datum(myData)         //Populate the &lt;svg&gt; element with chart data...
             .call(chart);          //Finally, render the chart!
@@ -36,35 +35,28 @@ $(document).ready(function(){
     });
   });
 
-  function sinAndCos() {
-    var sin = [],sin2 = [],
-        cos = [];
-  
-    //Data is represented as an array of {x,y} pairs.
-    for (var i = 0; i < 100; i++) {
-      sin.push({x: i, y: Math.sin(i/10)});
-      sin2.push({x: i, y: Math.sin(i/10) *0.25 + 0.5});
-      cos.push({x: i, y: .5 * Math.cos(i/10)});
+  function getData(res) {
+    var ret = []
+
+    var blood_types = ['O-', 'A-', 'B-', 'AB-', 'O+', 'A+', 'B+', 'AB+'];
+    
+    for(var i = 0; i < blood_types.length; ++i){
+      ret.push({
+          values: [],
+          key: blood_types[i]
+      });
     }
+
+    for(var i = 0; i < res.length; ++i){
+      var current = res[i];
+      var pos = blood_types.indexOf(current.type);
+      if(pos != -1){
+        ret[pos].values.push({x:new Date(current.date), y:parseFloat(current.days_remaining)});
+      }
+    }
+    
   
     //Line chart data should be sent as an array of series objects.
-    return [
-      {
-        values: sin,      //values - represents the array of {x,y} data points
-        key: 'Sine Wave', //key  - the name of the series.
-        color: '#ff7f0e'  //color - optional: choose your own line color.
-      },
-      {
-        values: cos,
-        key: 'Cosine Wave',
-        color: '#2ca02c'
-      },
-      {
-        values: sin2,
-        key: 'Another sine wave',
-        color: '#7777ff',
-        area: true      //area - set to true if you want this line to turn into a filled area chart.
-      }
-    ];
+    return ret;
   }
 });
