bfd04a3080de6ef498e408c80cd5ba9686a091c3
Paul Cowie <paul.cowie@ntlworld.com>
2016-03-29 20:01:23 +0100

    Add focus chart

--- scripts/graph.js
+++ scripts/graph.js
@@ -4,7 +4,7 @@
 // Placeholder from http://nvd3.org/examples/line.html
 function startgraph(){
    nv.addGraph(function() {
-      var chart = nv.models.lineChart()
+      var chart = nv.models.lineWithFocusChart()
                       .margin({left: 100, right:50})  //Adjust chart margins to give the x-axis some breathing room.
                       .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
                       .showLegend(true)        //Show the legend, allowing users to turn on/off line series.
@@ -13,12 +13,23 @@ function startgraph(){
       ;
 
       chart.xAxis     //Chart x-axis settings
-           .axisLabel('Date')
            .tickFormat(function(d) { return d3.time.format('%Y-%m-%d')(new Date(d)); });
 
-      chart.yAxis     //Chart y-axis settings
-           .axisLabel('Days Remaining')
+      chart.x2Axis     //Chart x-axis settings
+           .tickFormat(function(d) { return d3.time.format('%Y-%m-%d')(new Date(d)); });
+
+      chart.yAxis     //chart y-axis settings
+           .axisLabel('days remaining')
            .tickFormat(d3.format('.1f'));
+
+      chart.y2Axis     //chart y-axis settings
+           .tickFormat(d3.format('.1f'));
+
+      function svg_height() {
+        return parseFloat(getComputedStyle(document.getElementById("chart")).height);
+      }
+
+      chart.focusHeight(0.2 * svg_height());
 
       /* Done setting the chart up? Time to render it!*/
       var myData = getData();    //You need data...
@@ -28,7 +39,9 @@ function startgraph(){
            .call(chart);           //Finally, render the chart!
 
       //Update the chart when window resizes.
-      nv.utils.windowResize(function() { chart.update(); });
+      nv.utils.windowResize(function() {
+         chart.focusHeight(0.2 * svg_height());
+         chart.update(); });
       return chart;
   });
 };
@@ -65,4 +78,4 @@ function getData() {
   return ret;
 }
 
-startgraph();
+document.onload = startgraph();
