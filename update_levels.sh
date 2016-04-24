3fd33322e2d9b5076a7d1f4ca18743595fcbe587
paul <paul.cowie@ntlworld.com>
2016-04-24 19:31:31 +0100

    Add safety check on update script

--- update_levels.sh
+++ update_levels.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 git pull
-./app/run.py --csv >> ./_data/blood_stats.csv
+
+new_csv="$(./app/run.py --csv)"
+new_date="$(echo "$new_csv" | tail -1 - | cut -d ',' -f 1 -)"
+last_date="$(tail -1 ./_data/blood_stats.csv | cut -d ',' -f 1 -)"
+
+if [[ "$new_date" == "$last_date" ]]; then
+    echo "Error: Already updated today"
+    exit 1
+fi
+
+echo "$new_csv" >> ./_data/blood_stats.csv
 git add -u
 git commit -m "AUTOMATIC UPDATE"
 git push origin gh-pages
